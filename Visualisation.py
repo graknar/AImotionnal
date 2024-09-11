b2ae39240442cd97fada0a83473ed88c1fe7517d
Thomas Dumont <graknarlorque@gmail.com>
2024-09-11 15:37:37 +0000

    💩plot_surface

--- Visualisation.py
+++ Visualisation.py
@@ -1,27 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
-def plot_4d_dataframe(data):
+def plot_4D_dataframe(data):
+    # Assumons que les dimensions de 'data' sont (x, y, z, color)
+    x_dim, y_dim, z_dim, color_dim = data.shape
+    
+     # Extraire les données pour la coupe spécifiée
+    X, Y = np.meshgrid(np.arange(x_dim), np.arange(y_dim))
+    Z = np.mean(data, axis=3).mean(axis=0)  # Utilise la moyenne ou une autre opération pour obtenir Z
+    
+    # Crée une figure
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    x = np.arange(data.shape[0])
-    y = np.arange(data.shape[1])
-    z = np.arange(data.shape[2])
-    c = data.flatten()
-
-    x, y, z = np.meshgrid(x, y, z)
-    x = x.flatten()
-    y = y.flatten()
-    z = z.flatten()
-
-    # Ajuster la taille de c pour qu'elle corresponde à x et y
-    if len(c) != len(x):
-        c = c[:len(x)]
-
-    ax.scatter(x, y, z, c=c, cmap='viridis')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
+    # Crée le plot de surface
+    surf = ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
+    
+    # Ajoute une barre de couleur pour la quatrième dimension
+    fig.colorbar(surf, ax=ax, label='Fourth Dimension Value')
+    
+    # Définit les labels des axes
+    ax.set_xlabel('X Dimension')
+    ax.set_ylabel('Y Dimension')
+    ax.set_zlabel('Z Dimension')
+    
+    # Affiche le plot
+    plt.title(f'Combined Surface')
     plt.show()
