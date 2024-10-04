ffdc728ce23dce51efc3477f6a7f288ad504a8e5
Thomas Dumont <graknarlorque@gmail.com>
2024-10-04 17:37:45 +0000

    💩 small change in the data visualization

--- streamlit.py
+++ streamlit.py
@@ -8,13 +8,18 @@ from Function.Visualisation import plot_4D_dataframe
 def generate_data():
     return np.random.normal(-1, 1, size=(10, 10, 10, 10))
 
+def random_alteration(data):
+    return data + np.random.normal(-1,1, size=data.shape)
+
 # Configuration de Streamlit
 st.title('Affichage de plot en direct via un stream')
 placeholder = st.empty()
 
+df = generate_data()
+
 # Boucle pour mettre à jour les données et le graphique en temps réel
 while True:
-    df = generate_data()
+    df=random_alteration(df)
     fig = plot_4D_dataframe(df)
     placeholder.pyplot(fig)
     time.sleep(1)
